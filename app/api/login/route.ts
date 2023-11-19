ad98c9ff1afc53031a0393668557d2248bc61238
Marcus <34757275+Macronius@users.noreply.github.com>
2023-11-19 15:19:40 -0600

    load admin user, refactor db connection

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,18 +1,15 @@
-// create a POST endpoint to login the user
+// POST endpoint to login the user
 
-import { getClient } from "@/db";
-import {NextResponse} from "next/server";
+import { sql } from "@/db";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    //
-    const json = await request.json();
-
-    //
-    const client = await getClient();
-    await client.connect();
-    const res = await client.query("select id, username, password from users where username ilike $1", [json.username]);  // the i in front of 'ilike' indicates case-insensitivity
-    await client.end();
-
-    //
-    return NextResponse.json({data: res.rows[0]});
+  const json = await request.json();
+  //
+  const res = await sql(
+    "select id, username, password from users where username ilike $1",
+    [json.username]
+  );
+  //
+  return NextResponse.json({ data: res.rows[0] });
 }
--- db.ts
+++ db.ts
@@ -1,18 +1,31 @@
-import { Client } from "pg";
-import {loadEnvConfig} from '@next/env';
+import { Client, QueryResult } from "pg";
+import { loadEnvConfig } from "@next/env";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
-export async function getClient() {
+// refactor: specify return type of Client
+export async function getClient(): Promise<Client> {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
 
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
-      });
+  return client;
+}
 
-      return client;
+export async function sql(
+  sql: string,
+  values?: Array<any>
+): Promise<QueryResult<any>> {
+  const client = await getClient();
+  //
+  await client.connect();
+  const res = await client.query(sql, values);
+  await client.end();
+  //
+  return res;
 }
