ba20a9368e5645d7f9c108c4f152ae1a0148c61e
Marcus <34757275+Macronius@users.noreply.github.com>
2023-11-19 10:54:37 -0600

    bcrypt

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,6 +1,7 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
 import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
 
 const projectDir = process.cwd();
 // loadEnvConfig takes what is in .env.local file and loads as environment variables to make available using process.env
@@ -21,15 +22,17 @@ async function loadFakeData(numUsers: number = 10) {
   try {
     //
     await client.query("begin");
-    
+
     // CREATE FAKE USERS
     for (let i = 0; i < numUsers; i++) {
+      // generate random salt value for password hash
+      const saltRounds = 10;
+      const hash = await bcrypt.hash("password1234", saltRounds);
       // parameterized query
       await client.query(
         "insert into public.users (username, password, avatar) values ($1, $2, $3)",
-        [faker.internet.userName(), "password", faker.image.avatar()]
+        [faker.internet.userName(), hash, faker.image.avatar()]
       );
-      // TODO: secure solution for password
     }
 
     // CREATE FAKE POSTS
@@ -37,7 +40,7 @@ async function loadFakeData(numUsers: number = 10) {
       "select id from public.users order by created_at desc limit $1",
       [numUsers]
     );
-    console.log(res.rows);
+    // console.log(res.rows);
 
     for (const row of res.rows) {
       for (let i = 0; i < Math.ceil(Math.random() * 10); i++) {
@@ -50,16 +53,16 @@ async function loadFakeData(numUsers: number = 10) {
 
     // CREATE FAKE FOLLOWS
     for (const row1 of res.rows) {
-        for (const row2 of res.rows) {
-            if (row1.id != row2.id) {
-                if (Math.random() > 0.5) {
-                    await client.query(
-                        "insert into follows (user_id, follower_id) values ($1, $2)",
-                        [row1.id, row2.id]
-                    )
-                }
-            }
+      for (const row2 of res.rows) {
+        if (row1.id != row2.id) {
+          if (Math.random() > 0.5) {
+            await client.query(
+              "insert into follows (user_id, follower_id) values ($1, $2)",
+              [row1.id, row2.id]
+            );
+          }
         }
+      }
     }
 
     await client.query("commit");
@@ -71,12 +74,16 @@ async function loadFakeData(numUsers: number = 10) {
   }
 }
 
-const numUsers = parseInt(process.argv[2]) || 2;
-
-console.log(process.argv[0]);
-console.log(process.argv[1]);
+// 
+// console.log("===== 1 =====");
+// console.log(process.argv);
+// console.log(process.argv[0]);
+// console.log(process.argv[1]);
+// console.log(process.argv[2]);
+// console.log("===== 2 =====");
+const numUsers = parseInt(process.argv[2]) || 12;
+// QUESTION: what is at process.argv[2] ?
 
 console.log(`loading ${numUsers} fake users`);
 loadFakeData(numUsers);
-
-loadFakeData();
+// NOTE: cannot load fake data if the tables do not exist already
