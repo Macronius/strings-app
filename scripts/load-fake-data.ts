b42114a61268c7ea5a576972068a8b627ba81af0
Marcus <34757275+Macronius@users.noreply.github.com>
2023-11-18 20:08:45 -0600

    complete load fake data script

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,29 +1,82 @@
 import { Client } from "pg";
-import {loadEnvConfig} from '@next/env';
+import { loadEnvConfig } from "@next/env";
+import { faker } from "@faker-js/faker";
 
 const projectDir = process.cwd();
 // loadEnvConfig takes what is in .env.local file and loads as environment variables to make available using process.env
 loadEnvConfig(projectDir);
 
 async function loadFakeData(numUsers: number = 10) {
-    console.log(`executing load fake data. generating ${numUsers} users.`);
+  // INSTANTIATE CLIENT INSTANCE OF DATABASE
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
 
-    // locally - use key-value pairs
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
-
-    });
-
-    await client.connect();
-
-    const res = await client.query("select 1");
-    console.log(res);
+  // TRANSACTION "not necessary because this is development script"
+  await client.connect();
+  try {
+    //
+    await client.query("begin");
     
+    // CREATE FAKE USERS
+    for (let i = 0; i < numUsers; i++) {
+      // parameterized query
+      await client.query(
+        "insert into public.users (username, password, avatar) values ($1, $2, $3)",
+        [faker.internet.userName(), "password", faker.image.avatar()]
+      );
+      // TODO: secure solution for password
+    }
+
+    // CREATE FAKE POSTS
+    const res = await client.query(
+      "select id from public.users order by created_at desc limit $1",
+      [numUsers]
+    );
+    console.log(res.rows);
+
+    for (const row of res.rows) {
+      for (let i = 0; i < Math.ceil(Math.random() * 10); i++) {
+        await client.query(
+          "insert into public.posts (user_id, content) values ($1, $2)",
+          [row.id, faker.lorem.sentence()]
+        );
+      }
+    }
+
+    // CREATE FAKE FOLLOWS
+    for (const row1 of res.rows) {
+        for (const row2 of res.rows) {
+            if (row1.id != row2.id) {
+                if (Math.random() > 0.5) {
+                    await client.query(
+                        "insert into follows (user_id, follower_id) values ($1, $2)",
+                        [row1.id, row2.id]
+                    )
+                }
+            }
+        }
+    }
+
+    await client.query("commit");
+  } catch (err) {
+    await client.query("rollback");
+    throw err;
+  } finally {
     await client.end();
+  }
 }
+
+const numUsers = parseInt(process.argv[2]) || 2;
+
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+
+console.log(`loading ${numUsers} fake users`);
+loadFakeData(numUsers);
 
 loadFakeData();
