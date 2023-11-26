8abf5efcb93d14f50fa48f0f128372154c92beeb
Marcus <34757275+Macronius@users.noreply.github.com>
2023-11-26 15:32:06 -0600

    correct 500 error to unauthenticated 401

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,33 @@
 import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function middleware(request: NextRequest) {
+  //
+  const pathname = request.nextUrl.pathname;
+  // inject authentication middleware into any of the following paths
+  const authenticatedAPIRoutes = [
+    pathname.startsWith("/api/users"),
+    pathname.startsWith("/api/posts"),
+  ];
+  //
+  if (authenticatedAPIRoutes.includes(true)) {
+    const cookie = request.cookies.get("jwt-token");
     //
-    const pathname = request.nextUrl.pathname;
-    // I think this means any route beginning with /api/users will be preceeded by this middleware function
-    const authenticatedAPIRoutes = [pathname.startsWith("/api/users")];
-    //
-    if (authenticatedAPIRoutes.includes(true)) {
-        const cookie = request.cookies.get("jwt-token");
-        //
-        if (!cookie || !cookie.value) {
-            return NextResponse.json({error: "unauthenticated"}, {status: 401})
-        }
-        //
-        try {
-            const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
-            //
-            await jwtVerify(cookie.value, secret);
-        } catch (err) {
-            console.error(err);
-            return NextResponse.json({error: "internal server error"}, {status: 500})
-        }
+    if (!cookie || !cookie.value) {
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
     }
+    //
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+      //
+      await jwtVerify(cookie.value, secret);
+    } catch (err) {
+      console.error(err);
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
+    }
+  }
 }
 
 export const config = {
-    matcher: "/:path*",
-}
+  matcher: "/:path*",
+};
